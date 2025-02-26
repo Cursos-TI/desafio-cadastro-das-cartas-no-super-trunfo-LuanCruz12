c936d8633d29ec473d1158e7f0f8e92c68d6b720
LuanCruz12 <lua.cruz1205@gmail.com>
2025-02-25 21:12:42 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-int população 50 000 00
-float área; 10,000
-float PIB; 1,5% 
-int número de pontos turísticos; 500
+int população = 20,000;
+float área = 10.000;
+float PIB = 1.5;
+int número de pontos turísticos = 5000;
 
 }
 
